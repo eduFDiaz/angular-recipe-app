3dc105b0ca0fe20c7c230d1cfea7cf5f11337794
Eduardo Antonio Fernandez Diaz <fernandez9000@gmail.com>
2019-08-12 20:17:24 -0500

    Recipe model edited to look more realistic :)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,9 +9,9 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('Test recipe','Pork recipe','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('Test recipe','Pork recipe','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('Test recipe','Pork recipe','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    new Recipe('Baked lamb','Exquisite baked lamb ribs','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('Roasted potatoes','Delicious roasted potatoes slices','https://thecozyapron.com/wp-content/uploads/2018/03/dijon-roasted-potatoes_thecozyapron_1.jpg'),
+    new Recipe('Boiled eggs','Boiled eggs with salt and garlic','https://live.staticflickr.com/7838/46602248784_1b5fcc443d_b.jpg')
   ];
 
   constructor() { }
