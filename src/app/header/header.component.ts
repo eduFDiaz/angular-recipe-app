f9461047ab76e6a5bd20cff7dc8a849818e0257f
Eduardo Antonio Fernandez Diaz <fernandez9000@gmail.com>
2019-08-13 20:26:08 -0500

    passing Recipe data using property binding

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,15 +7,15 @@ import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 })
 
 export class HeaderComponent implements OnInit{
-    //The event will be accessible from outside (app parent component)
+    // The event will be accessible from outside (app parent component)
     @Output() featureSelected = new EventEmitter<string>();
 
     ngOnInit(){
     }
 
     onSelect(feature: string){
-        //featureSelected will emit either those two strings
-        //that are given at the links at the bar
+        // featureSelected will emit either those two strings
+        // that are given at the links at the bar
         this.featureSelected.emit(feature);
     }
 }
