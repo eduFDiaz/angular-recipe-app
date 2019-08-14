651fe7ec8fde371c5450e3b1e04a941e61563a4d
Eduardo Antonio Fernandez Diaz <fernandez9000@gmail.com>
2019-08-13 22:44:16 -0500

    passing the data down to item detail using Event and property binding (combined)

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,6 @@
+import { EventEmitter } from '@angular/core';
 import { Recipe } from './../../recipe.model';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
@@ -8,10 +9,14 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelected() {
+    this.recipeSelected.emit();
+  }
 }
