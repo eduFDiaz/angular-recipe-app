900978cc6d591d0bb27a12e2df74ae2f8dd56ff4
Eduardo Antonio Fernandez Diaz <fernandez9000@gmail.com>
2019-08-14 21:28:53 -0500

    using new RecipeService to populate RecipeListComponen

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { RecipeService } from './../recipe.service';
 import { Recipe } from './../recipe.model';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
@@ -8,15 +9,12 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
   @Output() RecipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('Baked lamb Chops', 'Exquisite baked lamb ribs', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('Roasted potatoes', 'Delicious roasted potatoes slices', 'https://thecozyapron.com/wp-content/uploads/2018/03/dijon-roasted-potatoes_thecozyapron_1.jpg'),
-    new Recipe('Boiled eggs in Garlic', 'Boiled eggs with salt and garlic', 'https://live.staticflickr.com/7838/46602248784_1b5fcc443d_b.jpg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,14 @@
-export class RecipeService {
+import { Recipe } from './recipe.model';
 
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('Baked lamb Chops', 'Exquisite baked lamb ribs', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('Roasted potatoes', 'Delicious roasted potatoes slices', 'https://thecozyapron.com/wp-content/uploads/2018/03/dijon-roasted-potatoes_thecozyapron_1.jpg'),
+    new Recipe('Boiled eggs in Garlic', 'Boiled eggs with salt and garlic', 'https://live.staticflickr.com/7838/46602248784_1b5fcc443d_b.jpg')
+  ];
+
+  getRecipes() {
+    // Slice returns an exact copy of the array, not a reference
+    return this.recipes.slice();
+  }
 }
