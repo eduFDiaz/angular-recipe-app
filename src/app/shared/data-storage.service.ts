154fd632f1d4af17b6eed2be42de7d7dc1fa914e
Eduardo Antonio Fernandez Diaz <fernandez9000@gmail.com>
2019-08-21 12:35:57 -0500

    bug caused when recipes with no ingredients where loaded is fixed now

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,6 +4,8 @@ import { HttpClient } from '@angular/common/http';
 import { RecipeService } from './../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 
+import { map } from 'rxjs/operators';
+
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
   constructor(private http: HttpClient, private recipeService: RecipeService) {
@@ -21,7 +23,12 @@ export class DataStorageService {
 
   fetchRecipes() {
     this.http
-    .get<Recipe[]>('https://recipe-book-api-9afeb.firebaseio.com/recipes.json')
+      .get<Recipe[]>('https://recipe-book-api-9afeb.firebaseio.com/recipes.json')
+      .pipe(map(recipes => {
+        return recipes.map(recipe => {
+          return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
+        });
+      }))
       .subscribe(recipes => {
         this.recipeService.setRecipes(recipes);
       }, error => {
