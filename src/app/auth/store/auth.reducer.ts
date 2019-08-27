a5fe6ec2e72ddf888c998315e137c69bfa6024a0
Eduardo Antonio Fernandez Diaz <fernandez9000@gmail.com>
2019-08-27 14:45:42 -0500

    login and logout actions added to the auth reducer

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,3 +1,4 @@
+import * as AuthActions from './auth.actions';
 import { User } from '../user.model';
 
 export interface State {
@@ -8,6 +9,19 @@ const initialState: State = {
   user: null
 };
 
-export function authReducer(state = initialState, action) {
+export function authReducer(state = initialState, action: AuthActions.AuthActions) {
+  switch (action.type) {
+    case AuthActions.LOGIN:
+      const newUser = new User(
+        action.payload.email,
+        action.payload.userId,
+        action.payload.token,
+        action.payload.expirationDate);
+      return { ...state, user: newUser};
+    case AuthActions.LOGOUT:
+      return {...state, user: null};
+    default:
+      break;
+  }
   return state;
 }
